8dd186d04a9af8917757132a2eff1702b75b3cd9
Warren Brown <warren@katapultmedia.com>
2020-03-01 12:50:00 -0600

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HompageComponent } from './hompage/hompage.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HompageComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HompageComponent } from './hompage/hompage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HompageComponent
   ],
   imports: [
     BrowserModule,
